b534ee449d48f90f102192c77895dad0659acc97
Ka-Houl <646676256@qq.com>
2021-08-12 01:20:00 +0800

    feat: 更新防抖方法

--- 防抖.js
+++ 防抖.js
@@ -7,15 +7,40 @@
  * @return {*}
  */
 function debounce(func, wait) {
-  var timeout;
+  var timeout
+  // var context = this;
+  return function () {
+    var context = this
+    var args = arguments
+
+    clearTimeout(timeout)
+    timeout = setTimeout(function () {
+      func.apply(context, args)
+    }, wait)
+  }
+}
+
+// 立刻执行
+// 第四版
+function debounce(func, wait, immediate) {
+  var timeout
 
   return function () {
-      var context = this;
-      var args = arguments;
+    var context = this
+    var args = arguments
 
-      clearTimeout(timeout)
-      timeout = setTimeout(function(){
-          func.apply(context, args)
-      }, wait);
+    if (timeout) clearTimeout(timeout)
+    if (immediate) {
+      // 如果已经执行过，不再执行
+      var callNow = !timeout
+      timeout = setTimeout(function () {
+        timeout = null
+      }, wait)
+      if (callNow) func.apply(context, args)
+    } else {
+      timeout = setTimeout(function () {
+        func.apply(context, args)
+      }, wait)
+    }
   }
 }
